1aadc9acd616c61d85cca07a30da55674f8412a3
sitthikonwarit <sitthikonwarit@gmail.com>
2026-04-08 10:30:01 +0700

    router

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 const app = express();
 const server = http.createServer(app);
